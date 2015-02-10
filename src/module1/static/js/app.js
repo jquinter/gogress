566ec7710c17c8e82277bb62b46655211a9900e6
jquinter <jquinter@gmail.com>
2015-02-09 21:14:16 -0300

    Fix para despligue responsive; agrego dos rutas para la app: /dev_invo y ruta por default

--- src/module1/static/js/app.js
+++ src/module1/static/js/app.js
@@ -44,9 +44,12 @@ app.config(function($authProvider, $mdThemingProvider, $routeProvider, $location
       templateUrl: 'tmpl/op_edit.html',
       controller: 'OperationController'
     })
-    .otherwise({
+    .when('/dev_info/', {
       templateUrl: 'tmpl/default.html',
       controller: 'DefaultController'
+    })
+    .otherwise({
+      templateUrl: 'tmpl/nelson.html',
     })
   $resourceProvider.defaults.stripTrailingSlashes = false;
   $locationProvider.html5Mode(true);
